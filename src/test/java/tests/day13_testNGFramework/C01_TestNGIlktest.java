fa621fd12dc7f34e37e307e660a4a2c4d3eb6c50
aymurat111 <aymurat111@gmail.com>
2022-10-31 10:52:40 +0100

    junit

--- src/test/java/tests/day13_testNGFramework/C01_TestNGIlktest.java
+++ src/test/java/tests/day13_testNGFramework/C01_TestNGIlktest.java
@@ -1,2 +1,21 @@
-package tests.day13_testNGFramework;public class C01_TestNGIlktest {
-}
+package tests.day13_testNGFramework;
+
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C01_TestNGIlktest extends TestBase {
+    @Test(priority = 10)
+    public void ilkTest() {
+        driver.get( "https://amazon.de" );
+    }
+    @Test(priority = 11)
+    public void ikinciTest() {
+        driver.get( "https://wisequarter.com" );
+    }
+    @Test(priority = 12)
+    public void ucubcuTest() {
+        driver.get( "https://youtube.com" );
+    }
+    }
+
+
