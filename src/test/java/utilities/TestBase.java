08778fdd4972bd00c83a6cb9c4980794bbc44302
aymurat111 <aymurat111@gmail.com>
2022-10-31 20:01:12 +0100

    tt

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -2,19 +2,21 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import java.time.Duration;
 public class TestBase {
     protected WebDriver driver;
-    @BeforeMethod
+    @BeforeClass
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         driver=new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().window().maximize();
     }
-    @AfterMethod
+    @AfterClass
     public void teardown(){
         driver.close();
     }
