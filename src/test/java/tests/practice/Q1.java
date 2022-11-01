6ddd18ea5f044446023815cdfa0f7b5f7bea9753
aymurat111 <aymurat111@gmail.com>
2022-11-01 20:18:31 +0100

    test

--- src/test/java/tests/practice/Q1.java
+++ src/test/java/tests/practice/Q1.java
@@ -1,7 +1,34 @@
 package tests.practice;
 
-import utilities.TestBase;
+import org.testng.annotations.Test;
 
-public class Q1 extends TestBase {
-
+public class Q1 {
+    /*TestNG test methodlarini alfabetik siraya gore run eder,
+    yukaridan asagiya dogru degil.
+    > priority annotation testlre oncelik vermek icin kullanilir,
+     kucuk olan numara daha once calisir.
+    > priority : TestNG testlerinde , testler konsola alfabetik sira ile yazdirilir.
+    > priority default degeri sifirdir.
+    > enabled = false methodu : Testi gormezden gelmek icin @Test 'in yanina yazilir.
+     */
+    @Test(priority = 300)
+    public void b(){
+    System.out.println("b");
+}
+    @Test(priority = -300)
+    public void a(){
+        System.out.println("a");
+}
+    @Test(priority = 301)
+    public void c() {
+        System.out.println( "c" );
+    }
+    @Test(enabled = false)
+    public void d() {
+        System.out.println( "d" );
+        }
+    @Test(dependsOnMethods = "c")
+    public void e() {
+        System.out.println( "e" );
+    }
 }
