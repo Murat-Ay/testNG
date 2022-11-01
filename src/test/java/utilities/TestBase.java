d34a20b5d0b56d0ecd93f1b37137428131b3ae31
aymurat111 <aymurat111@gmail.com>
2022-11-01 12:26:01 +0100

    testNG

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -1,23 +1,44 @@
 package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 public class TestBase {
     protected WebDriver driver;
-    @BeforeClass
+    @BeforeMethod
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         driver=new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().window().maximize();
     }
-    @AfterClass
+    @AfterMethod
     public void teardown(){
         driver.close();
+    }
+    public void bekle(int saniye){
+        try {
+            Thread.sleep(saniye*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void fotografCek(){
+        TakesScreenshot tss= (TakesScreenshot) driver;
+        File resimDosyasi= new File("target/screnShots/tumSayfaResmi.jpeg");
+        File geciciResim=tss.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(geciciResim,resimDosyasi);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
