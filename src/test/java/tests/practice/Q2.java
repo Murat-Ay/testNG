bbc43fe811900e5b15010fd4856fc68f66a6bd4d
aymurat111 <aymurat111@gmail.com>
2022-11-03 09:51:45 +0100

    testNG

--- src/test/java/tests/practice/Q2.java
+++ src/test/java/tests/practice/Q2.java
@@ -4,14 +4,12 @@ import org.testng.annotations.Test;
 import utilities.TestBase;
 
 public class Q2 extends TestBase {
-    /*
-    - Birbirine bagimli testler olusturun.
+    /* - Birbirine bagimli testler olusturun.
     - beforeClass olusturup setUp ayarlarini yapin.
     - Birbirine bagimli testlr olusturarak ;
         =>ilk once facebook'a gidin
         =>sonra facebook'a bagimli olarak google'a gidin
-        =>daha sonra google'a bagimli olarak amazon'a gidin
-    -driver'i kapatin.    */
+        =>daha sonra google'a bagimli olarak amazon'a gidin driver'i kapatin.    */
 
     @Test(dependsOnMethods = "facebook")
     public void google() {
