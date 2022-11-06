33e28c741172bb684c057234341541498d9840ef
aymurat111 <aymurat111@gmail.com>
2022-11-06 21:39:44 +0100

    pomXML

--- src/test/java/tests/day16_pageObjectModel/C01_ConfigReaderKullanimi.java
+++ src/test/java/tests/day16_pageObjectModel/C01_ConfigReaderKullanimi.java
@@ -1,18 +1,18 @@
 package tests.day16_pageObjectModel;
 
+import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.AmazonPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
-
 public class C01_ConfigReaderKullanimi {
     @Test
     public void method1(){
         Driver.getDriver().get( ConfigReader.getProperty( "amazonUrl" ) );
         AmazonPage amazonPage=new AmazonPage();
-        amazonPage.aramaKutusu.sendKeys(ConfigReader.getProperty( "amazonAramaKelimesi" ));
+        amazonPage.aramaKutusu.sendKeys(ConfigReader.getProperty( "amazonAramaKelimesi" )+ Keys.ENTER);
 
 String expectedKelime=ConfigReader.getProperty( "amazonAramaKelimesi" );
 String actualAramaSonucu=amazonPage.aramaSonucElementi.getText();
