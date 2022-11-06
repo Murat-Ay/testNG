9134612fad02dbd96273162f3e0873f9da365a3a
aymurat111 <aymurat111@gmail.com>
2022-11-06 10:15:50 +0100

    Driver

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,7 +3,8 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import java.time.Duration;
 
 public class Driver {
@@ -14,14 +15,29 @@ public class Driver {
 
         if(driver==null) {// method ilk cagrildiginda driver degeri atanmamis oldugundan deger ata
             // sonraki calistirmalarda degeri atanmis oldugundan 18.satir calismayacak
-          String browser =ConfigReader.getProperty( "browser" )
-            switch (browser){
-              case "chrome":
-          }
-          WebDriverManager.chromedriver().setup();
-          driver = new ChromeDriver();
-        }
 
+            String browser= ConfigReader.getProperty("browser");
+
+            switch (browser){
+                case "chrome" :
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver=new FirefoxDriver();
+                    break;
+                case "safari" :
+                    WebDriverManager.safaridriver().setup();
+                    driver=new SafariDriver();
+                    break;
+
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+
+            }
+        }
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().window().maximize();
