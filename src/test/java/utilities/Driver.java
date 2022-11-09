f613a55bdc92c0fd9871c92e72117fb11e497f31
aymurat111 <aymurat111@gmail.com>
2022-11-09 13:13:17 +0100

    singleton

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -5,12 +5,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+
 import java.time.Duration;
 
 public class Driver {
 
+    private Driver(){
+        // Singleton pattern konsepti ile
+        // Driver class'indan obje olusturmayi engellemek icin bu cons.'i olusturduk
+    }
+
     static WebDriver driver;
     public static WebDriver getDriver(){
+
 
         if(driver==null) {// method ilk cagrildiginda driver degeri atanmamis oldugundan deger ata
             // sonraki calistirmalarda degeri atanmis oldugundan 18.satir calismayacak
@@ -34,6 +41,7 @@ public class Driver {
                 default:
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
+
             }
         }
 
@@ -41,6 +49,7 @@ public class Driver {
         driver.manage().window().maximize();
         return driver;
     }
+
     public static void closeDriver(){
         if (driver!=null){
             driver.close();
