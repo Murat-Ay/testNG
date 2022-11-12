d6a642db01453f1781cffe4a48c7f2ed1245e051
aymurat111 <aymurat111@gmail.com>
2022-11-12 08:49:38 +0100

    testNG

---
+++ src/test/java/tests/day18_testNGReports_paralelTesting/C01_ReusableMethodsWindowshandle.java
@@ -0,0 +1,5 @@
+package tests.day18_testNGReports_paralelTesting;
+
+public class C01_ReusableMethodsWindowshandle {
+    //Elemental Selenium: Receive a Free, Weekly Tip on Using Selenium like a Pro
+}
