578ffa29cd656d113e67816e81ae871ed9375737
aymurat111 <aymurat111@gmail.com>
2022-11-12 09:07:30 +0100

    testNG

--- src/test/java/tests/day18_testNGReports_paralelTesting/C02_ReusableMethodsElementList.java
+++ src/test/java/tests/day18_testNGReports_paralelTesting/C02_ReusableMethodsElementList.java
@@ -1,10 +1,17 @@
 package tests.day18_testNGReports_paralelTesting;
 
 import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
 
 public class C02_ReusableMethodsElementList {
     @Test
     public void test01(){
+        // amazon anasayfaya gidin
+        Driver.getDriver().get( ConfigReader.getProperty( "amazonUrl" ) );
+        // arama kutusunun yanindaki dropdown menuyu locate edin
+        // dropdown menudeki tum options listesini olusturup
+        // menude Electronics basligi oldugunu test edin
 
 
     }
