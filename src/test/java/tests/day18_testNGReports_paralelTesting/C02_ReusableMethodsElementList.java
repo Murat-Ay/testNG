25ed031133d5a26b44c4b9aedc90485be7ee93fa
aymurat111 <aymurat111@gmail.com>
2022-11-12 09:01:51 +0100

    testNG

---
+++ src/test/java/tests/day18_testNGReports_paralelTesting/C02_ReusableMethodsElementList.java
@@ -0,0 +1,11 @@
+package tests.day18_testNGReports_paralelTesting;
+
+import org.testng.annotations.Test;
+
+public class C02_ReusableMethodsElementList {
+    @Test
+    public void test01(){
+
+
+    }
+}
