21017f217011fb6237e7bfaef38f5d26f970fae5
aymurat111 <aymurat111@gmail.com>
2022-11-13 09:57:02 +0100

    cross

---
+++ src/test/java/tests/day19_CrossBrowserTests/C01_AmazonSearchTest.java
@@ -0,0 +1,12 @@
+package tests.day19_CrossBrowserTests;
+
+import org.testng.annotations.Test;
+import utilities.TestBaseCross;
+
+public class C01_AmazonSearchTest extends TestBaseCross {
+    @Test
+    public void test01(){
+
+
+    }
+}
